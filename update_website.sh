33d1075087b4ec52fda160569ba03ffaa6e0f484
iiilfaaa <at.iiilfaaa@gmail.com>
2017-12-22 01:32:32 +0700

    Update website Jum Des 22 01:32:32 WIB 2017

--- update_website.sh
+++ update_website.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 buster generate
-<<<<<<< HEAD
-git add .
+git add --all
 git commit -m "Update website $(date)"
 git push -u origin master
-=======
-git add --all
-git commit -m "Update on the website at $(date)"
-git push -u origin gh-pages 
->>>>>>> 3bee31d08d939fc2d838607c2d4e1fd5dacff618
